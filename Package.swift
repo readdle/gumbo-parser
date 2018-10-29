a9954df1c6e63c9fcb01f03488f9bb6647d3b893
Viacheslav Savchenko <vs.savchenko@readdle.com>
2018-10-29 09:57:06 +0200

    Dev: update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,9 +1,12 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
     name: "SwiftGumbo",
     products: [
-        .library(name: "SwiftGumbo", type: .static, targets: ["SwiftGumbo"])
+        .library(name: "SwiftGumbo", type: .static, targets: ["SwiftGumbo"]),
+    ],
+    dependencies: [
     ],
     targets: [
     	.target(name: "SwiftGumbo", path: "./src")
